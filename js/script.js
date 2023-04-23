8957b4bb71d49c2b664d71f2f8e8b4c6e775fce5
lneoa <leonacheun@gmail.com>
2023-04-22 20:36:45 -0400

    UAG, garden pilot, colophon

--- js/script.js
+++ js/script.js
@@ -151,3 +151,14 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+
+
+// Overlay for colophon
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+} 
